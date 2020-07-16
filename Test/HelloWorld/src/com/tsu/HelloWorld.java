10cc7ec79e5ccf36f6f33117c5dc5c9d3491d802
zhouhao <1940836858@qq.com>
2020-07-16 10:31:35 +0800

    添加到本地7

--- Test/HelloWorld/src/com/tsu/HelloWorld.java
+++ Test/HelloWorld/src/com/tsu/HelloWorld.java
@@ -2,11 +2,9 @@ package com.tsu;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("你好");
-        System.out.println("你好");
+
         System.out.println("花开富贵");
-        System.out.println("你好");
-        System.out.println("添加");
-        System.out.println("你好·");
+
+        System.out.println("haha");
     }
 }

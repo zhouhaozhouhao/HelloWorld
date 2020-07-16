715a02d59a933c66a63d5028bbdcec0c6e0e123e
zhouhao <1940836858@qq.com>
2020-07-16 09:27:17 +0800

    添加到本地3

--- Test/HelloWorld/src/com/tsu/HelloWorld.java
+++ Test/HelloWorld/src/com/tsu/HelloWorld.java
@@ -4,5 +4,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("你好");
         System.out.println("你好");
+        System.out.println("花开富贵");
     }
 }
